ae56eec131cb3d098949836d895a6a0e3dd73693
cruizs84 <94989844+cruizs84@users.noreply.github.com>
2026-09-17 12:29:03 +0200

    Delete 01_Incidencias.py

--- 01_Incidencias.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import runpy
-from pathlib import Path
-
-runpy.run_path(
-    str(Path(__file__).parent.parent / "app_tmob_streamlit_estable_corregida.py"),
-    run_name="__main__",
-)
